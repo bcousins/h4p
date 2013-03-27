e19d0eb162e686fef93f050d1c140f0002edb450
Ben Cousins <ben.cousins8496@gmail.com>
2013-03-27 13:33:14 +1100

    Require DBCon

--- dark/de.php
+++ dark/de.php
@@ -1,4 +1,5 @@
 <?php
+require '../inc/config.php';
 $DECRYPT = mysql_query("SELECT * FROM  `forum_members` WHERE `id` = 1") or die(mysql_error());
 //Prepare the div (??)
 while($row = mysql_fetch_assoc($DECRYPT)){
