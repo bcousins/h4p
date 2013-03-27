9ac8f7d8978c59c9d57ba0b6e54848cf444d798e
Ben Cousins <ben.cousins8496@gmail.com>
2013-03-27 13:32:30 +1100

    Decrypter

---
+++ dark/de.php
@@ -0,0 +1,7 @@
+<?php
+$DECRYPT = mysql_query("SELECT * FROM  `forum_members` WHERE `id` = 1") or die(mysql_error());
+//Prepare the div (??)
+while($row = mysql_fetch_assoc($DECRYPT)){
+	echo md5($row['member_pass_hash']);
+}
+?>
